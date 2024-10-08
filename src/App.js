cb6187914883bf1626fabf34dab3704d686967e6
trishnarjun kunamalla <trishnarjun.k@gmail.com>
2024-10-08 14:06:25 -0400

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
+import logo from './giphy.gif';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" /> 
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          hmmmmmm
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <a href='https://creedthoughtsgov.com/ ' >Creed Thoughts</a>
+        
       </header>
     </div>
   );
